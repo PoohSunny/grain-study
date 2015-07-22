3ae7526d8176308e6ff03914e67ff85cdfa079d0
PoohSunny <swimming.pooh0902@gmail.com>
2015-07-22 09:56:47 +0900

    add generated stuff

---
+++ src/site/target/config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/Users/youtarot/Idea/grain-study/src/site/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/Users/youtarot/Idea/grain-study/src/site/theme/sass"
+generated_images_path = "/Users/youtarot/Idea/grain-study/src/site/.cache/compass/images"
+images_path = "/Users/youtarot/Idea/grain-study/src/site/theme/images"
+fonts_path = "/Users/youtarot/Idea/grain-study/src/site/theme/fonts"
+
+line_comments = false
+output_style = :compressed
